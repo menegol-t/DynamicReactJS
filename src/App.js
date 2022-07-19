6c1ffecde9d0498b916d751df44bc71f9990926e
Tomas Menegol <tomas.menegol@gmail.com>
2022-07-19 00:27:54 -0300

    react usando nav bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css" 
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1> Aubier: Lentes de madera.</h1> 
-      </header>
+      <NavBar />  
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,53 @@
+import "./NavBar.css"
+import "./General.css"
+
+const NavBar = ()=> {
+    return (
+    <>
+        <div className="div__header" id="cartel">
+            <p className="txtCenter txtSmall txtWhite"> <span className="txtWhite ">Premium designer eyewear.</span></p>
+        </div>
+        
+        <nav className="navbar navbar-expand-lg navbar-light bgBrown nav_sitcky" id="nav__sticky">
+
+            <div className="container-fluid">
+
+                <a className="navbar-brand" href="index.html">
+                    <h1 className="txtLeft fontSpecial animate__pulse ms-3">AUBIER</h1>
+                </a>
+
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav m-auto mb-2 mb-lg-0 txtMed">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="index.html">Inicio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="pages/productos.html">Productos</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="pages/novedades.html">Novedades</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="pages/nosotros.html">Nosotros?</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link active" href="pages/faq.html">FAQ</a>
+                        </li>
+                    </ul>
+
+                    <form id="formBusqueda" className="d-flex me-3 form__width">
+                        <input id="inputBusqueda" className="me-2 txtSmall" type="text" placeholder="Haga click en busqueda..." aria-label="Search" />
+                        <button id="inputBoton" className="btn btn-outline-light btn-lg" type="button"> <span className="txtSmall">Búqueda</span></button>
+                    </form>
+
+                </div>
+            </div>
+        </nav>
+    </>
+    )
+}
+export default NavBar

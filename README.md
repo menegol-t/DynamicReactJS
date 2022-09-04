5ebbb51e106cb6d1e1ccc86a53f264fc9f80936e
Tomas Menegol <tomas.menegol@gmail.com>
2022-09-04 00:58:19 -0300

    update al readme

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 Powered by [ReactJS](https://github.com/facebook/create-react-app) & [Firestore](https://firebase.google.com/docs/firestore). 
 
-Features from [Bootstrap V5](https://getbootstrap.com/docs/5.0/getting-started/introduction/), [React-Toastify](https://fkhadra.github.io/react-toastify/introduction), [SweetAlert2-React](https://sweetalert2.github.io/) & [React-Router-DOM V6](https://reactrouter.com/en/main).
+Features from [Bootstrap V5 & React-Bootstrap](https://getbootstrap.com/docs/5.0/getting-started/introduction/), [React-Toastify](https://fkhadra.github.io/react-toastify/introduction), [SweetAlert2-React](https://sweetalert2.github.io/) & [React-Router-DOM V6](https://reactrouter.com/en/main).
 
 ---
-## Deployments
+## Deployment
 
 
 
@@ -38,12 +38,17 @@ See the section about [deployment](https://facebook.github.io/create-react-app/d
 ![Demonstration on how to briwse through the landing](https://firebasestorage.googleapis.com/v0/b/aubier-coderhousereact.appspot.com/o/chiquitos%2Fcategory.gif?alt=media&token=e829b4c6-1b86-4668-ab96-a00fab3dff04)
 
 ---
-* Counting items to purchase:
+* Counting items to purchase, validating stock and adding to cart:
 
 ![Demonstration on how the item counting works](https://firebasestorage.googleapis.com/v0/b/aubier-coderhousereact.appspot.com/o/chiquitos%2Fcounter.gif?alt=media&token=32e2b62e-b353-4251-81fd-5d10e4aa8a6f)
 
 ---
 * Cart usage with items (it's disabled when empty):
 
-![Demonstration on how to use the cart](https://firebasestorage.googleapis.com/v0/b/aubier-coderhousereact.appspot.com/o/chiquitos%2Fcart.gif?alt=media&token=4b6ded5b-2835-40eb-b8df-04d051b83741)
+![Demonstration on how to use the cart](https://firebasestorage.googleapis.com/v0/b/aubier-coderhousereact.appspot.com/o/chiquitos%2Fcart.gif?alt=media&token=43cf262d-2aab-4b00-bdbb-d70b0d851315)
+
+---
+* Checkout proccess checks against missing datafields, mismatching emails and gives you a purchase Id obtained from firebase:
+
+![Demonstration of checkout process](https://firebasestorage.googleapis.com/v0/b/aubier-coderhousereact.appspot.com/o/chiquitos%2Fcheckout.gif?alt=media&token=d9a7fe70-ec53-4aaa-a768-fda751277fb3)
 
--- src/components/Cart/CartContainer.js
+++ src/components/Cart/CartContainer.js
@@ -2,83 +2,20 @@ import React from 'react';
 import { CartContext } from "../../context/CartContext"
 import { useContext, useEffect } from "react";
 import CartItems from './CartItems';
-import { addDoc, collection, getFirestore } from "firebase/firestore";
-import Swal from "sweetalert2"
 
 const CartContainer = () => {
 
     const {cart} = useContext(CartContext)
     
-    const {finalPrice} = useContext(CartContext);
+    const {totalPrice} = useContext(CartContext)
 
     const {cleanCart} = useContext(CartContext)
 
-    const impuestoIva = finalPrice * 0.21
+    const {impuestoIva} = useContext(CartContext)
 
-    const precioFinal = impuestoIva + finalPrice
+    const {precioFinal} = useContext(CartContext)
 
-    const userWhoOrdered = async() => {
-      const { value: formValues } = await Swal.fire({
-        title: 'Ingresa tus datos para finalizar la compra!',
-        html:
-          '<input id="swal-input1" class="swal2-input" placeholder="Nombre" type="text" required>' +
-          '<input id="swal-input2" class="swal2-input" placeholder="Apellido" type="text" required>' +
-          '<input id="swal-input3" class="swal2-input -webkit-appearance: none; -moz-appearance: none; appearance: none;" placeholder="Telefono simplificado" type="number" min="0" onkeydown="javascript: return [`Backspace`,`Delete`,`ArrowLeft`,`ArrowRight`].includes(event.code) ? true : !isNaN(Number(event.key)) && event.code!==`Space`">' +
-          '<input id="swal-input4" class="swal2-input" placeholder="Email">' +
-          '<input id="swal-input5" class="swal2-input" placeholder="Ingresa tu Email nuevamente" type="email">',
-        focusConfirm: false,
-        preConfirm: () => {
-          return [
-            document.getElementById('swal-input1').value,
-            document.getElementById('swal-input2').value,
-            document.getElementById('swal-input3').value,
-            document.getElementById('swal-input4').value,
-            document.getElementById('swal-input5').value
-          ]
-        }
-      })
-      if (formValues.includes("")) {
-        Swal.fire({
-          icon: 'warning',
-          title: 'Datos incompletos',
-          text: "Asegurate de completar todos los campos."
-        })
-      }else if(formValues[3] !== formValues[4] || !formValues[3].includes("@")){
-        Swal.fire({
-          icon: 'warning',
-          title: 'Email no valido',
-          text: "Recuerda ingresalo las dos veces igual, y con '@'."
-        })
-      }else{
-        let user = {
-          name: formValues[0],
-          surname: formValues[1],
-          phone: formValues[2],
-          email: formValues[3]
-        }
-        sendOrder(user)
-      }}
-
-    const sendOrder = (user) => {
-      const newOrder = {
-        buyer: user, 
-        items: cart, 
-        date: new Date().toString(),
-        total: precioFinal,
-        status: "generada"
-      } 
-      const db = getFirestore()
-      const orderCollection = collection(db, "order")
-      addDoc(orderCollection, newOrder).then(({id}) => 
-      {console.log({id})
-      Swal.fire({
-        icon: 'success',
-        title: `Compra exitosa, ${newOrder.buyer.name}! Id: ${id}`,
-        showConfirmButton: true
-      })
-      }).then(cleanCart).catch((error) => console.error(error))
-      console.log(newOrder);
-    }
+    const {userDataOrder} = useContext(CartContext)
 
     useEffect(() => {
       window.scrollTo({top: 0, behavior: 'auto'})
@@ -117,14 +54,14 @@ const CartContainer = () => {
             <div className="col-md-6 offset-md-3">
               <div className="d-flex justify-content-between" >
                 <p className="mb-2">Total (sin IVA)</p>
-                <p className="mb-2">${finalPrice}</p>
+                <p className="mb-2">${totalPrice}</p>
               </div>
               <hr className="my-4"/>
               <div className="d-flex justify-content-between mb-4" >
                 <p className="mb-2">Impuestos (IVA)</p>
                 <p className="mb-2">${impuestoIva}</p>
               </div>
-              <button type="button" className="btn bgBrown btn-block btn-lg" onClick={userWhoOrdered}>
+              <button type="button" className="btn bgBrown btn-block btn-lg" onClick={userDataOrder}>
                 <div className="d-flex justify-content-between">
                   <span className='txtSmall2'>Terminar la compra, total:</span>
                   <span className='txtSmall2'>${precioFinal}</span>
